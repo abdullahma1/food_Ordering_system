4347820c9e1846d46996ff9b1f2be9adfa0e4a26
abdullahma1 <abdullahjawed900@gmail.com>
2023-12-21 12:00:00 +0500

    Homepage bottom bar

--- src/AppRouters.tsx
+++ src/AppRouters.tsx
@@ -1,9 +1,10 @@
 import { Navigate, Route, Routes } from "react-router-dom"
 import Layout from './layouts/layout'
+import Homepage from "./pages/Homepage"
 const AppRoutes = () =>{
     return(
         <Routes>
-            <Route path="/" element={<Layout childern={"Home page"}></Layout>}/>
+            <Route path="/" element={<Layout childern={<Homepage/>}></Layout>}/>
             <Route path="/user-profile" element={<span>User</span>}/>
             <Route path="*" element={<Navigate to="/"/>}/>
         </Routes>
