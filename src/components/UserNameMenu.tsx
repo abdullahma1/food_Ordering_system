25cdacb04e18ba1898cb1494bc281ddb6416b96c
abdullahma1 <abdullahjawed900@gmail.com>
2024-01-01 12:00:00 +0500

    UserNameMenu

--- src/components/UserNameMenu.tsx
+++ src/components/UserNameMenu.tsx
@@ -1,8 +1,28 @@
+import { useAuth0 } from '@auth0/auth0-react'
+import { DropdownMenuTrigger, DropdownMenu, DropdownMenuContent, DropdownMenuItem } from '@radix-ui/react-dropdown-menu'
+import { CircleUserRound } from 'lucide-react'
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const UserNameMenu = () => {
+  const { user } = useAuth0();
   return (
-    <DropdownMenu></DropdownMenu>
+    <DropdownMenu>
+      <DropdownMenuTrigger className='flex items-center px-3 font-bold hover:text-orange-500 gap-2'>
+        <CircleUserRound className='text-orange-500'>
+          {user?.email}
+        </CircleUserRound>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuItem>
+          <Link to='/user-profile' className='font-bold hover:text-orange-500'>
+            User Profile
+          </Link>
+        </DropdownMenuItem>
+
+      </DropdownMenuContent>
+
+    </DropdownMenu>
   )
 }
 
--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -5,7 +5,7 @@ const Homepage = () => {
   return (
     <div className='flex flex-col gap-12 ml-16 mr-16'>
       <div className='bg-white rounded-lg shadow-md py-8 flex flex-col gap-5 text-center -mt-20'>
-        <h1 className="text-5xl font-bold text-orange-600">Get your order today</h1>
+        <h1 className="md:text-5xl text-3xl font-bold text-orange-600">Get your order today</h1>
         <span className="text-2xl tracking-tight font-dancing">Food is love and now your love is just a click away</span>
       </div>
       <div className="grid md:grid-cols-2 gap-5 bg-[hsl(19.37deg_100%_49.8%)]">
