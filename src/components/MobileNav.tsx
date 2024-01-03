b6930400851aa684998c5a67d613038989c93ed8
abdullahma1 <abdullahjawed900@gmail.com>
2024-01-03 12:00:00 +0500

    Auth and login completed

--- src/components/MobileNav.tsx
+++ src/components/MobileNav.tsx
@@ -3,6 +3,7 @@ import { SheetTrigger,Sheet, SheetTitle, SheetDescription, SheetContent } from '
 import { Separator } from './ui/separator'
 import { Button } from './ui/button'
 import { useAuth0 } from '@auth0/auth0-react'
+import MobileNavLinks from './ui/MobileNavLinks'
 
 const MobileNav = () => {
   const { isAuthenticated , loginWithRedirect , user} = useAuth0();
@@ -15,13 +16,13 @@ const MobileNav = () => {
         <SheetContent className='space-y-3'>
         <SheetTitle>
           {isAuthenticated ? <span className='flex items-center font-bold gap-2'>
-            <CircleUserRound className='flex-1 font-bold bg-orange-500 '></CircleUserRound>
+            <CircleUserRound className='flex-1 font-bold  text-orange-500'></CircleUserRound>
          {user?.email} </span> : <span>Welcome to FOODEATS</span> }
             
         </SheetTitle>
         <Separator/>
-        <SheetDescription className='flex'>
-          {isAuthenticated ? <MobileNavLinks /> :  <Button className='flex-1 font-bold bg-orange-500'>Login</Button> }
+        <SheetDescription className='flex flex-col gap-4'>
+          {isAuthenticated ? <MobileNavLinks /> :  <Button className='flex-1 font-bold bg-orange-500' onClick={()=>loginWithRedirect()}>Login</Button> }
            
         </SheetDescription>
         </SheetContent>
